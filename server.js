e81d3384f9987cceff195988b50e730e1886c29e
selimmouelhi <slmmlh30@gmail.com>
2017-03-01 21:32:25 +0100

    postinga message and saving to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser =require("body-parser");
+var mongo = require("mongodb").MongoClient;
+var databas;
 app.use(bodyParser.json());
 
 
@@ -12,10 +14,18 @@ app.use(function(req,res,next){
 
 });
 
-app.post(`/api/message`,function(req,res){
-console.log(req.body);
-res.status(200);
+app.post("/api/message",function(req,res){
+  console.log(req.body);
+  databas.collection("messages").insertOne(req.body);
+  res.status(200);
 
+});
+mongo.connect("mongodb://localhost:27017/test", function(err,db){
+     if(!err)
+        {            console.log("we are connected to mongo");
+            databas = db;
+
+}
 });
 var serv= app.listen(5000,function(){
 console.log(`listening on port `,serv.address().port);
