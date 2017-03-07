68b9b2963871ab9693b5681a68ec00d77284ff2e
selimmouelhi <slmmlh30@gmail.com>
2017-03-07 23:48:38 +0100

    create a login endpoint

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors);
 app.get('/api/message', message.get);
 app.post('/api/message',checkAuthenticated, message.post );
 app.post('/auth/register',auth.register );
+app.post('/auth/login', auth.login);
 
 //Connection
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
